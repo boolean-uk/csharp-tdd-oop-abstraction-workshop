6ccb03cb6b8d3dc2eec764b86e2c1fed3024c2ee
uerbzr <nigel.sibbert@gmail.com>
2023-03-31 16:24:23 +0100

    Add project files.

---
+++ Boolean.CSharp.Main/Core.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace Boolean.CSharp.Main
+{
+    public abstract class Shape
+    {
+        public abstract double area();
+    }
+
+
+    public class Circle : Shape
+    {
+        public Circle()
+        {
+                
+        }
+        private double radius;
+        public Circle(double r)
+        {
+            radius = r;
+        }
+        public override double area()
+        {
+            return (3.14 * radius * radius);
+        }
+    }
+    public class Square : Shape
+    {
+        private double side;
+        public Square(double s)
+        {
+            side = s;
+        }
+        public override double area()
+        {
+            return (side * side);
+        }
+    }
+    public class Triangle : Shape
+    {
+        private double tbase;
+        private double theight;
+        public Triangle(double b, double h)
+        {
+            tbase = b;
+            theight = h;
+        }
+        public override double area()
+        {
+            return (0.5 * tbase * theight);
+        }
+    }
+    public class Core
+    {
+
+    }
+}
---
+++ Boolean.CSharp.Main/Program.cs
@@ -0,0 +1,14 @@
+﻿using Boolean.CSharp.Main;
+using System.Drawing;
+
+Circle c = new Circle(5.0);
+Console.ForegroundColor = c.cc;
+Console.WriteLine("Area of Circle = {0}", c.area());
+Square s = new Square(2.5);
+Console.WriteLine("Area of Square = {0}", s.area());
+Triangle t = new Triangle(2.0, 5.0);
+Console.WriteLine("Area of Triangle = {0}", t.area());
+Console.ForegroundColor = ConsoleColor.White;
+
+
+Console.ReadLine();
